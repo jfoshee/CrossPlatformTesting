6559d6c921e8d16f01cfa090c27b89eec25f06fb
jfoshee <jacobf@gmail.com>
2012-08-09 16:57:22 -0500

    Eliminated unnecessary test

--- VisualStudioTesting/WidgetTest.cs
+++ VisualStudioTesting/WidgetTest.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FluentAssertions;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StandardClassLibrary;
 
 namespace VisualStudioTesting
@@ -7,12 +7,6 @@ namespace VisualStudioTesting
     [TestClass]
     public class WidgetTest
     {
-        [TestMethod]
-        public void DefaultConstruction()
-        {
-            var subject = new Widget();
-        }
-
         [TestMethod]
         public void FluentTest()
         {
