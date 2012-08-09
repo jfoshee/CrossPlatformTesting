8fb0b2ff54f89fdae62604bd5920ee6d43554b11
jfoshee <jacobf@gmail.com>
2012-08-09 16:11:43 -0500

    Added a standard visual studio testing project

---
+++ VisualStudioTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VisualStudioTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Unplugged Development")]
+[assembly: AssemblyProduct("VisualStudioTesting")]
+[assembly: AssemblyCopyright("Copyright © Unplugged Development 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("387c799a-0c62-4ecf-9fe2-69c0ae3adfd0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VisualStudioTesting/WidgetTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StandardClassLibrary;
+
+namespace VisualStudioTesting
+{
+    [TestClass]
+    public class WidgetTest
+    {
+        [TestMethod]
+        public void DefaultConstruction()
+        {
+            var subject = new Widget();
+        }
+    }
+}
